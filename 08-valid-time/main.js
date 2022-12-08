52769ba2e3b0de2a854288833d069e624f3cbdb2
OtterSloth <76695983+OtterSloth@users.noreply.github.com>
2022-12-08 05:39:39 -0500

    Day 8 challenge complete

--- 08-valid-time/main.js
+++ 08-valid-time/main.js
@@ -1,52 +1,63 @@
 function validTime(str) {
-    //  write code here.
+  //  write code here.
+  let isValidHour = false;
+  let isValidMinute = false;
+  const [hourString, minuteString] = str.split(':');
+  const hourValue = parseInt(hourString);
+  const minuteValue = parseInt(minuteString);
+
+  if (hourValue >= 0 && minuteValue >= 0) {
+    if (hourValue <= 23) isValidHour = true;
+    if (minuteValue <= 59) isValidMinute = true;
+    if (isValidHour && isValidMinute) return true;
+
+    return false;
+  }
 }
 
-
-
 /**
-* Test Suite 
-*/
+ * Test Suite
+ */
 describe('validTime()', () => {
-    it('returns true for valid time', () => {
-        // arrange
-        const str = '13:58';
-        
-        // act
-        const result = validTime(str);
+  it('returns true for valid time', () => {
+    // arrange
+    const str = '13:58';
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(true);
-    });
+    // act
+    const result = validTime(str);
 
-    it('returns false when invalid hours', () => {
-        // arrange
-        const str = '25:51';
-        
-        // act
-        const result = validTime(str);
+    // log
+    console.log('result 1: ', result);
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(false);
-    });
+    // assert
+    expect(result).toBe(true);
+  });
 
-    it('returns false when invalid minutes', () => {
-        // arrange
-        const str = '02:76';
-        
-        // act
-        const result = validTime(str);
+  it('returns false when invalid hours', () => {
+    // arrange
+    const str = '25:51';
 
-        // log
-        console.log("result 1: ", result);
-        
-        // assert
-        expect(result).toBe(false);
-    });
+    // act
+    const result = validTime(str);
+
+    // log
+    console.log('result 1: ', result);
+
+    // assert
+    expect(result).toBe(false);
+  });
+
+  it('returns false when invalid minutes', () => {
+    // arrange
+    const str = '02:76';
+
+    // act
+    const result = validTime(str);
+
+    // log
+    console.log('result 1: ', result);
+
+    // assert
+    expect(result).toBe(false);
+  });
 });
